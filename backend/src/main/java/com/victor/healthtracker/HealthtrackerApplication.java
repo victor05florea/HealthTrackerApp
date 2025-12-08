eb2a205d13879e3a9e05534a3852f5fdf73defd2
Florea Victor <victor05florea@gmail.com>
2025-12-08 03:08:02 +0200

    Adaugare proiect Spring Boot in backend

---
+++ backend/src/main/java/com/victor/healthtracker/HealthtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.victor.healthtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthtrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victor/healthtracker/HealthtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.healthtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
